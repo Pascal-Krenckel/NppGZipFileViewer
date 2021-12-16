a158c941a87545bf8047b0a5131e3d48fefc8cd3
Pascal Krenckel <krenckel@krenckel.eu>
2021-12-16 23:03:07 +0100

    Added options to exclude specific files. Usefull to stop compressing gz files.

--- NppGZipFileViewer/FileTracker.cs
+++ NppGZipFileViewer/FileTracker.cs
@@ -8,12 +8,15 @@ namespace NppGZipFileViewer
     {
         HashSet<IntPtr> zippedFiles = new HashSet<IntPtr>();
         Dictionary<IntPtr, string> zippedFilePathes = new Dictionary<IntPtr, string>();
+
+        HashSet<IntPtr> excludedFiles = new HashSet<IntPtr> ();
         public void Include(IntPtr id, StringBuilder path)
         {
             Include(id, path.ToString());
         }
         public void Include(IntPtr id, string path)
         {
+            excludedFiles.Remove(id);
             zippedFiles.Add(id);
             if (zippedFilePathes.ContainsKey(id))
                 zippedFilePathes.Add(id, path);
@@ -22,7 +25,16 @@ namespace NppGZipFileViewer
 
         public void Remove(IntPtr id) { zippedFiles.Remove(id); zippedFilePathes.Remove(id); }
 
+        public void Exclude(IntPtr id)
+        {
+            if(zippedFiles.Remove(id)) zippedFilePathes.Remove(id);
+
+            excludedFiles.Add(id);
+        }
+
         public bool Contains(IntPtr id) { return zippedFiles.Contains(id); }
+
+        public bool IsExcluded(IntPtr id) { return excludedFiles.Contains(id); }
 
         public string GetStoredPath(IntPtr id) { zippedFilePathes.TryGetValue(id, out string path); return path; }
 
