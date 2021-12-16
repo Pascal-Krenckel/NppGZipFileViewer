8c513d30ef118d209e0bec6a4661346f7ca33150
Pascal Krenckel <krenckel@krenckel.eu>
2021-12-16 23:47:26 +0100

    Exclude now also needs a path and will update the file pathes.

--- NppGZipFileViewer/FileTracker.cs
+++ NppGZipFileViewer/FileTracker.cs
@@ -7,40 +7,50 @@ namespace NppGZipFileViewer
     public class FileTracker
     {
         HashSet<IntPtr> zippedFiles = new HashSet<IntPtr>();
-        Dictionary<IntPtr, string> zippedFilePathes = new Dictionary<IntPtr, string>();
+        Dictionary<IntPtr, string> filePathes = new Dictionary<IntPtr, string>();
 
         HashSet<IntPtr> excludedFiles = new HashSet<IntPtr> ();
         public void Include(IntPtr id, StringBuilder path)
         {
             Include(id, path.ToString());
         }
+
+        public void Exclude(IntPtr id, StringBuilder path)
+        {
+            Exclude(id, path.ToString());
+        }
+
         public void Include(IntPtr id, string path)
         {
             excludedFiles.Remove(id);
             zippedFiles.Add(id);
-            if (zippedFilePathes.ContainsKey(id))
-                zippedFilePathes.Add(id, path);
-            else zippedFilePathes[id] = path;
+            if (filePathes.ContainsKey(id))
+                filePathes.Add(id, path);
+            else filePathes[id] = path;
         }
-        public void Exclude(IntPtr id)
+        public void Exclude(IntPtr id, string path)
         {
-            if(zippedFiles.Remove(id)) zippedFilePathes.Remove(id);
+            zippedFiles.Remove(id);
 
             excludedFiles.Add(id);
+            if (filePathes.ContainsKey(id))
+                filePathes.Add(id, path);
+            else filePathes[id] = path;
         }
 
 
         public void Remove(IntPtr id)
         {
-            if (zippedFiles.Remove(id)) zippedFilePathes.Remove(id);
+            zippedFiles.Remove(id);
             excludedFiles.Remove(id);
+            filePathes.Remove(id);
         }
 
         public bool IsIncluded(IntPtr id) { return zippedFiles.Contains(id); }
 
         public bool IsExcluded(IntPtr id) { return excludedFiles.Contains(id); }
 
-        public string GetStoredPath(IntPtr id) { zippedFilePathes.TryGetValue(id, out string path); return path; }
+        public string GetStoredPath(IntPtr id) { filePathes.TryGetValue(id, out string path); return path; }
 
 
     }
