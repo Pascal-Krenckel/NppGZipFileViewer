24f4f7452d86919246430310e36b1b3ba4d45769
Pascal Krenckel <krenckel@krenckel.eu>
2021-12-17 00:26:58 +0100

    Delete GetFilePath, NppGateway is used now.

--- NppGZipFileViewer/NppGZipFileViewerHelper.cs
+++ NppGZipFileViewer/NppGZipFileViewerHelper.cs
@@ -9,14 +9,24 @@ namespace NppGZipFileViewer
 {
     static class NppGZipFileViewerHelper
     {
-        public static StringBuilder GetFilePath(ScNotification notification)
+        internal static MemoryStream GetContentStream(ScNotification notification, StringBuilder path)
         {
-            StringBuilder path = new StringBuilder(Win32.MAX_PATH);
-            Win32.SendMessage(PluginBase.nppData._nppHandle, (uint)NppMsg.NPPM_GETFULLPATHFROMBUFFERID, notification.Header.IdFrom, path);
-            return path;
+            Win32.SendMessage(PluginBase.nppData._nppHandle, (uint)NppMsg.NPPM_SWITCHTOFILE, notification.Header.IdFrom, path);
+
+            int data_length = (int)Win32.SendMessage(PluginBase.GetCurrentScintilla(), SciMsg.SCI_GETLENGTH, 0, 0);
+            if (data_length <= 0)
+                return new MemoryStream();
+
+            var pData = Win32.SendMessage(PluginBase.GetCurrentScintilla(), SciMsg.SCI_GETCHARACTERPOINTER, 0, 0);
+            if (pData == IntPtr.Zero)
+                return new MemoryStream();
+            MemoryStream memoryStream = new MemoryStream();
+            memoryStream.SetLength(data_length);
+            Marshal.Copy(pData, memoryStream.GetBuffer(), 0, data_length);
+            return memoryStream;
         }
 
-        internal static MemoryStream GetContentStream(ScNotification notification, StringBuilder path)
+        internal static MemoryStream GetContentStream(ScNotification notification, string path)
         {
             Win32.SendMessage(PluginBase.nppData._nppHandle, (uint)NppMsg.NPPM_SWITCHTOFILE, notification.Header.IdFrom, path);
 
