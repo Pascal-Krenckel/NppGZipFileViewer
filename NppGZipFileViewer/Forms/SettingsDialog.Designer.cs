711d508c9df99eab2ca0b91613582581f3a640f1
Pascal Krenckel <krenckel@krenckel.eu>
2022-07-11 19:40:36 +0200

    reworked settings dialog

--- NppGZipFileViewer/Forms/SettingsDialog.Designer.cs
+++ NppGZipFileViewer/Forms/SettingsDialog.Designer.cs
@@ -29,26 +29,19 @@ namespace NppGZipFileViewer.Forms
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SettingsDialog));
-            this.lst_Suffixes = new System.Windows.Forms.ListBox();
             this.chk_DecompressAll = new System.Windows.Forms.CheckBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.btn_Add = new System.Windows.Forms.Button();
-            this.txt_Suffix = new System.Windows.Forms.TextBox();
-            this.btn_Delete = new System.Windows.Forms.Button();
             this.btn_OK = new System.Windows.Forms.Button();
             this.btn_Cancel = new System.Windows.Forms.Button();
+            this.chkListBoxCompressionAlg = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.btnUp = new System.Windows.Forms.Button();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.btnSettings = new System.Windows.Forms.Button();
+            this.btnDefault = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // lst_Suffixes
-            // 
-            this.lst_Suffixes.FormattingEnabled = true;
-            this.lst_Suffixes.ItemHeight = 20;
-            this.lst_Suffixes.Location = new System.Drawing.Point(32, 75);
-            this.lst_Suffixes.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.lst_Suffixes.Name = "lst_Suffixes";
-            this.lst_Suffixes.Size = new System.Drawing.Size(348, 304);
-            this.lst_Suffixes.TabIndex = 0;
             // 
             // chk_DecompressAll
             // 
@@ -61,43 +54,6 @@ namespace NppGZipFileViewer.Forms
             this.chk_DecompressAll.TabIndex = 1;
             this.chk_DecompressAll.Text = "Try to decompress all files";
             this.chk_DecompressAll.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(28, 50);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(103, 20);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "GZip-suffixes";
-            // 
-            // btn_Add
-            // 
-            this.btn_Add.Location = new System.Drawing.Point(167, 387);
-            this.btn_Add.Name = "btn_Add";
-            this.btn_Add.Size = new System.Drawing.Size(102, 31);
-            this.btn_Add.TabIndex = 3;
-            this.btn_Add.Text = "Add suffix";
-            this.btn_Add.UseVisualStyleBackColor = true;
-            this.btn_Add.Click += new System.EventHandler(this.btn_Add_Click);
-            // 
-            // txt_Suffix
-            // 
-            this.txt_Suffix.Location = new System.Drawing.Point(32, 389);
-            this.txt_Suffix.Name = "txt_Suffix";
-            this.txt_Suffix.Size = new System.Drawing.Size(129, 26);
-            this.txt_Suffix.TabIndex = 4;
-            // 
-            // btn_Delete
-            // 
-            this.btn_Delete.Location = new System.Drawing.Point(275, 387);
-            this.btn_Delete.Name = "btn_Delete";
-            this.btn_Delete.Size = new System.Drawing.Size(105, 31);
-            this.btn_Delete.TabIndex = 5;
-            this.btn_Delete.Text = "Delete";
-            this.btn_Delete.UseVisualStyleBackColor = true;
-            this.btn_Delete.Click += new System.EventHandler(this.btn_Delete_Click);
             // 
             // btn_OK
             // 
@@ -119,19 +75,81 @@ namespace NppGZipFileViewer.Forms
             this.btn_Cancel.Text = "Cancel";
             this.btn_Cancel.UseVisualStyleBackColor = true;
             // 
+            // chkListBoxCompressionAlg
+            // 
+            this.chkListBoxCompressionAlg.FormattingEnabled = true;
+            this.chkListBoxCompressionAlg.Location = new System.Drawing.Point(12, 102);
+            this.chkListBoxCompressionAlg.Name = "chkListBoxCompressionAlg";
+            this.chkListBoxCompressionAlg.Size = new System.Drawing.Size(368, 235);
+            this.chkListBoxCompressionAlg.TabIndex = 8;
+            this.chkListBoxCompressionAlg.SelectedIndexChanged += new System.EventHandler(this.chkListBoxCompressionAlg_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(179, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Compression algorithms";
+            // 
+            // btnUp
+            // 
+            this.btnUp.Enabled = false;
+            this.btnUp.Location = new System.Drawing.Point(32, 343);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(102, 31);
+            this.btnUp.TabIndex = 10;
+            this.btnUp.Text = "up";
+            this.btnUp.UseVisualStyleBackColor = true;
+            this.btnUp.Click += new System.EventHandler(this.btnUp_Click);
+            // 
+            // btnDown
+            // 
+            this.btnDown.Enabled = false;
+            this.btnDown.Location = new System.Drawing.Point(140, 343);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(102, 31);
+            this.btnDown.TabIndex = 11;
+            this.btnDown.Text = "down";
+            this.btnDown.UseVisualStyleBackColor = true;
+            this.btnDown.Click += new System.EventHandler(this.btnDown_Click);
+            // 
+            // btnSettings
+            // 
+            this.btnSettings.Enabled = false;
+            this.btnSettings.Location = new System.Drawing.Point(278, 343);
+            this.btnSettings.Name = "btnSettings";
+            this.btnSettings.Size = new System.Drawing.Size(102, 31);
+            this.btnSettings.TabIndex = 12;
+            this.btnSettings.Text = "Settings";
+            this.btnSettings.UseVisualStyleBackColor = true;
+            this.btnSettings.Click += new System.EventHandler(this.ComprSettingsClick);
+            // 
+            // btnDefault
+            // 
+            this.btnDefault.Location = new System.Drawing.Point(170, 441);
+            this.btnDefault.Name = "btnDefault";
+            this.btnDefault.Size = new System.Drawing.Size(102, 31);
+            this.btnDefault.TabIndex = 13;
+            this.btnDefault.Text = "Default";
+            this.btnDefault.UseVisualStyleBackColor = true;
+            this.btnDefault.Click += new System.EventHandler(this.btnDefault_Click);
+            // 
             // SettingsDialog
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(417, 489);
+            this.Controls.Add(this.btnDefault);
+            this.Controls.Add(this.btnSettings);
+            this.Controls.Add(this.btnDown);
+            this.Controls.Add(this.btnUp);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chkListBoxCompressionAlg);
             this.Controls.Add(this.btn_Cancel);
             this.Controls.Add(this.btn_OK);
-            this.Controls.Add(this.btn_Delete);
-            this.Controls.Add(this.txt_Suffix);
-            this.Controls.Add(this.btn_Add);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.chk_DecompressAll);
-            this.Controls.Add(this.lst_Suffixes);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -141,20 +159,22 @@ namespace NppGZipFileViewer.Forms
             this.Name = "SettingsDialog";
             this.ShowInTaskbar = false;
             this.Text = "SettingsDialog";
+            this.Load += new System.EventHandler(this.SettingsDialog_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.ListBox lst_Suffixes;
         private System.Windows.Forms.CheckBox chk_DecompressAll;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button btn_Add;
-        private System.Windows.Forms.TextBox txt_Suffix;
-        private System.Windows.Forms.Button btn_Delete;
         private System.Windows.Forms.Button btn_OK;
         private System.Windows.Forms.Button btn_Cancel;
+        private System.Windows.Forms.CheckedListBox chkListBoxCompressionAlg;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Button btnUp;
+        private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.Button btnSettings;
+        private System.Windows.Forms.Button btnDefault;
     }
 }
--- NppGZipFileViewer/Forms/SettingsDialog.cs
+++ NppGZipFileViewer/Forms/SettingsDialog.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Windows.Forms;
+using System.Xml.Serialization;
 
 namespace NppGZipFileViewer.Forms
 {
@@ -13,34 +15,120 @@ namespace NppGZipFileViewer.Forms
             Icon = Icon.FromHandle(NppGZipFileViewer.Properties.Resources.gzip_filled64.GetHicon());
         }
 
-        private void btn_Add_Click(object sender, EventArgs e)
-        {
-            lst_Suffixes.Items.Add(txt_Suffix.Text);
-            txt_Suffix.Text = "";
-        }
 
-        private void btn_Delete_Click(object sender, EventArgs e)
-        {
-            if (lst_Suffixes.SelectedIndex >= 0)
-                lst_Suffixes.Items.RemoveAt(lst_Suffixes.SelectedIndex);
-        }
+        Preferences preferences;
 
         public Preferences Preferences
-        {
+        {// ToDo:
             get
             {
-                return new Preferences(chk_DecompressAll.Checked, lst_Suffixes.Items.Cast<string>());
+                preferences.DecompressAll = chk_DecompressAll.Checked;
+                preferences.CompressionAlgorithms.Clear();
+
+                foreach (var chkCompr in chkListBoxCompressionAlg.CheckedItems)
+                    preferences.CompressionAlgorithms.Add(chkCompr as string);
+
+                return preferences;
             }
             set
             {
+                // clone preferences
+                preferences = Clone(value);
                 chk_DecompressAll.Checked = value.DecompressAll;
-                lst_Suffixes.Items.AddRange(value.Extensions.ToArray());
+                //lst_Suffixes.Items.AddRange(value.Extensions.ToArray());
+
+                var checkedItems = value.CompressionAlgorithms.ToArray();
+                var uncheckedItems = value.EnumerateCompressions().Select(alg => alg.CompressionAlgorithm).Where(name => !checkedItems.Contains(name)).ToArray();
+
+                chkListBoxCompressionAlg.Items.AddRange(checkedItems);
+                chkListBoxCompressionAlg.Items.AddRange(uncheckedItems);
+
+                for (int i = 0; i < checkedItems.Length; i++)
+                    SetItemCheckState(i, CheckState.Checked);
+                for (int i = 0; i < uncheckedItems.Length; i++)
+                    SetItemCheckState(i + checkedItems.Length, CheckState.Unchecked);
             }
+        }
+
+        private Preferences Clone(Preferences value)
+        {
+            // Since Preferences is serializable;
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Preferences));
+            using MemoryStream memoryStream = new();
+            xmlSerializer.Serialize(memoryStream, value);
+            memoryStream.Seek(0, SeekOrigin.Begin);
+            return xmlSerializer.Deserialize(memoryStream) as Preferences;
         }
 
         ~SettingsDialog()
         {
             Icon.Dispose();
+        }
+
+        private void SettingsDialog_Load(object sender, EventArgs e)
+        {
+            toolTip1.SetToolTip(chkListBoxCompressionAlg, "Changes the order of the compression algorithmns. \n" +
+                "Only effects 'Toogle Compression'. To disable a compression remove the file association, too");
+
+            toolTip1.SetToolTip(chk_DecompressAll, "If set all files will be decompressed regardless of the suffix.");
+
+
+        }
+
+        void SetItemCheckState(int index, CheckState checkState)
+        {
+            chkListBoxCompressionAlg.SetItemCheckState(index, checkState);
+        }
+
+        private void ComprSettingsClick(object sender, EventArgs e)
+        {
+            switch (chkListBoxCompressionAlg.SelectedItem)
+            {
+                case "gzip":
+                    GZipSettingsDialog gZipSettingsDialog = new GZipSettingsDialog();
+                    gZipSettingsDialog.GZipSettings = Preferences.GZipSettings;
+                    if (gZipSettingsDialog.ShowDialog() == DialogResult.OK)
+                        Preferences.GZipSettings = gZipSettingsDialog.GZipSettings;
+                    break;
+                case "bzip2":
+                    BZip2SettingsDialog bZip2SettingsDialog = new BZip2SettingsDialog();
+                    bZip2SettingsDialog.BZip2Settings = Preferences.BZip2Settings;
+                    if (bZip2SettingsDialog.ShowDialog() == DialogResult.OK)
+                        Preferences.BZip2Settings = bZip2SettingsDialog.BZip2Settings;
+                    break;
+            }
+        }
+
+        private void chkListBoxCompressionAlg_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            btnSettings.Enabled = chkListBoxCompressionAlg.SelectedIndex >= 0;
+            btnUp.Enabled = chkListBoxCompressionAlg.SelectedIndex > 0;
+            btnDown.Enabled = chkListBoxCompressionAlg.SelectedIndex >= 0 &&
+                chkListBoxCompressionAlg.SelectedIndex < (chkListBoxCompressionAlg.Items.Count - 1);
+        }
+
+        private void btnUp_Click(object sender, EventArgs e)
+        {
+            (chkListBoxCompressionAlg.Items[chkListBoxCompressionAlg.SelectedIndex],
+                chkListBoxCompressionAlg.Items[chkListBoxCompressionAlg.SelectedIndex - 1]) =
+                (chkListBoxCompressionAlg.Items[chkListBoxCompressionAlg.SelectedIndex - 1],
+                chkListBoxCompressionAlg.Items[chkListBoxCompressionAlg.SelectedIndex]);
+            chkListBoxCompressionAlg.SelectedIndex = chkListBoxCompressionAlg.SelectedIndex - 1;
+        }
+
+        private void btnDown_Click(object sender, EventArgs e)
+        {
+            (chkListBoxCompressionAlg.Items[chkListBoxCompressionAlg.SelectedIndex],
+                  chkListBoxCompressionAlg.Items[chkListBoxCompressionAlg.SelectedIndex + 1]) =
+              (chkListBoxCompressionAlg.Items[chkListBoxCompressionAlg.SelectedIndex + 1],
+                  chkListBoxCompressionAlg.Items[chkListBoxCompressionAlg.SelectedIndex]);
+            chkListBoxCompressionAlg.SelectedIndex = chkListBoxCompressionAlg.SelectedIndex + 1;
+        }
+
+        private void btnDefault_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("This will reset ALL settings.", "Warning", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning) == DialogResult.OK)
+                this.Preferences = Preferences.Default;
         }
     }
 }
