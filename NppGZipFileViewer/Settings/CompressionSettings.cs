f3d3ab172b6ada9609c085695e0f4e403394dd66
Pascal Krenckel <krenckel@krenckel.eu>
2022-07-11 19:37:12 +0200

    Added base class for compressions

---
+++ NppGZipFileViewer/Settings/CompressionSettings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NppGZipFileViewer.Settings;
+
+[Serializable]
+public abstract class CompressionSettings
+{
+    public List<string> Extensions { get; set; } = new List<string>();
+
+    public abstract string CompressionAlgorithm { get; }
+
+    public void Compress(Stream inStream, Stream outStream)
+    {
+        using var compressionStream = GetCompressionStream(outStream);
+        inStream.CopyTo(compressionStream);
+    }
+
+    public void Decompress(Stream inStream, Stream outStream)
+    {
+        using var compressionStream = GetDecompressionStream(inStream);
+        compressionStream.CopyTo(outStream);
+    }
+
+    public abstract Stream GetCompressionStream(Stream outStream);
+    public abstract Stream GetDecompressionStream(Stream inStream);
+
+    public override string ToString() => CompressionAlgorithm;
+
+}
