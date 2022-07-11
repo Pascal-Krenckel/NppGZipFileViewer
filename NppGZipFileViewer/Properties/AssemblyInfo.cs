84063b47e6d85cb69fc64bf7c653552521e395c1
Pascal Krenckel <krenckel@krenckel.eu>
2022-07-11 19:43:49 +0200

    Bump to v2.0.0

--- NppGZipFileViewer/Properties/AssemblyInfo.cs
+++ NppGZipFileViewer/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NppGZipFileViewer")]
-[assembly: AssemblyDescription("This is a Notepad++ plugin to open and store files in gzip format.")]
+[assembly: AssemblyDescription("This is a Notepad++ plugin to open and store files in gzip or bzip2 format.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Pascal Krenckel")]
 [assembly: AssemblyProduct("NppGZipFileViewer")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.2.*")]
-[assembly: AssemblyFileVersion("1.2.2.0")]
+[assembly: AssemblyVersion("2.0.0.*")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
