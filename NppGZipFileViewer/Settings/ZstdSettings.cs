fad5198f79e8733e414827f9e66247a811e82e7e
Pascal Krenckel <krenckel@krenckel.eu>
2023-10-15 23:32:21 +0200

    Added ZstandardCompression

---
+++ NppGZipFileViewer/Settings/ZstdSettings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NppGZipFileViewer.Settings
+{
+    [Serializable]
+    public class ZstdSettings : CompressionSettings
+    {
+        public int CompressionLevel { get; set; } = 0;
+        public int BufferSize { get; set; } = 0;
+
+        public override string CompressionAlgorithm => "Zstd";
+
+        public override Stream GetCompressionStream(Stream outStream)
+        {
+            ZstdSharp.CompressionStream stream = new ZstdSharp.CompressionStream(outStream,CompressionLevel,BufferSize,true);
+            return stream;
+        }
+        public override Stream GetDecompressionStream(Stream inStream)
+        {
+            ZstdSharp.DecompressionStream decompressionStream = new ZstdSharp.DecompressionStream(inStream,BufferSize,true);
+            return decompressionStream;
+        }
+    }
+}
